1b5cc003416bc2b0ba8929f41cf082751f4f46ba
Jake Carter <jhcarter.co@gmail.com>
2021-11-16 12:21:04 -0600

    Add OS-specific options

--- maintain.py
+++ maintain.py
@@ -28,11 +28,16 @@ def time_me(f):
 
 @time_me
 def ps(cmd, env=None):
-    if env is None:
-        result = run(["powershell", cmd], capture_output=True)
+    # Windows
+    # if env is None:
+    #     result = run(["powershell", cmd], capture_output=True)
     
-    else:
-        result = run(["powershell", cmd], env=env, capture_output=True)
+    # else:
+    #     result = run(["powershell", cmd], env=env, capture_output=True)
+
+    # Linux
+    if env is None: result = run(cmd, capture_output=True, shell=True)
+    else: result = run(cmd, env=env, capture_output=True, shell=True)
 
     return result
 
@@ -79,16 +84,24 @@ def release(version):
 
 # Tests cleaning and compiling example projects for target platforms.  If no targets, boards, projects, etc. are specified then it will auto-detect
 def test(targets=None, boards=None, projects=None):
-    MAXIM_PATH = "C:/MaximSDK"
-    LOG_DIR = os.getcwd()
+    env = os.environ.copy()
+    curplatform = platform.system()
 
     # Simulate the VS Code terminal by appending to the Path
-    env = os.environ.copy()
-    env["PATH"] = f"{MAXIM_PATH}/Tools/MinGW/msys/1.0/bin;{MAXIM_PATH}/Tools/OpenOCD;{MAXIM_PATH}/Tools/GNUTools/bin;{MAXIM_PATH}/Tools/xPack/riscv-none-embed-gcc/bin;" + env["PATH"]
+    MAXIM_PATH = ""
+    if curplatform == 'Linux':
+        MAXIM_PATH = "/home/jcarter/MaximSDK" # Linux
+        env["PATH"] = f"{MAXIM_PATH}/Tools/OpenOCD:/{MAXIM_PATH}/Tools/GNUTools/bin:{MAXIM_PATH}/Tools/xPack/riscv-none-embed-gcc/bin:" + env["PATH"] # Linux
+    elif curplatform == 'Windows':
+        MAXIM_PATH = "C:/MaximSDK" # Windows
+        env["PATH"] = f"{MAXIM_PATH}/Tools/MinGW/msys/1.0/bin;{MAXIM_PATH}/Tools/OpenOCD;{MAXIM_PATH}/Tools/GNUTools/bin;{MAXIM_PATH}/Tools/xPack/riscv-none-embed-gcc/bin;" + env["PATH"] # Windows
+    
+    LOG_DIR = os.getcwd()
 
     # Create log file
+    try: os.mkdir(f"{LOG_DIR}/buildlogs")
+    except FileExistsError: pass
     logfile = open(f"{LOG_DIR}/test.log", 'w')
-    ps(f"mkdir {LOG_DIR}/buildlogs")
     
     # Log system info
     log(timestamp(), logfile)
@@ -98,7 +111,7 @@ def test(targets=None, boards=None, projects=None):
     if targets is None:
         targets = []
 
-        for dir in os.scandir("C:/MaximSDK/Examples"):
+        for dir in os.scandir(f"{MAXIM_PATH}/Examples"):
             targets.append(dir.name) # Append subdirectories of Examples to list of target micros
 
         log(f"[TARGETS] Detected targets {targets}", logfile)
@@ -109,6 +122,11 @@ def test(targets=None, boards=None, projects=None):
 
     # Enforce alphabetical ordering
     targets = sorted(targets)
+
+    # Create subfolders for target-specific logfiles
+    for t in targets: 
+        try: os.mkdir(f"{LOG_DIR}/{t}")
+        except FileExistsError: pass
 
     # Track failed projects for end summary
     failed = []
@@ -121,9 +139,9 @@ def test(targets=None, boards=None, projects=None):
         # Get list of supported boards for this target.
         if boards is None:
             boards = []
-            for dirpath, subdirs, items in os.walk(f"C:/MaximSDK/Libraries/Boards/{target}"):
-                if "board.mk" in items:
-                    boards.append(dirpath.split('\\')[-1]) # Board string will be the last folder in the directory path
+            for dirpath, subdirs, items in os.walk(f"{MAXIM_PATH}/Libraries/Boards/{target}"):
+                if "board.mk" in items and curplatform == 'Linux': boards.append(dirpath.split('/')[-1]) # Linux
+                elif "board.mk" in items and curplatform == 'Windows': boards.append(dirpath.split('\\')[-1]) # Board string will be the last folder in the directory path # Windows
 
             log(f"[BOARDS] Detected {boards}", logfile)
 
@@ -136,7 +154,7 @@ def test(targets=None, boards=None, projects=None):
         # Get list of examples for this target.  If a Makefile is in the root directory it's an example.
         if projects is None:
             projects = []
-            for dirpath, subdirs, items in os.walk(f"C:/MaximSDK/Examples/{target}"):
+            for dirpath, subdirs, items in os.walk(f"{MAXIM_PATH}/Examples/{target}"):
                 if 'Makefile' in items:
                     projects.append(dirpath)  
 
@@ -150,28 +168,26 @@ def test(targets=None, boards=None, projects=None):
 
         # Test each project
         for project in projects:
-            project_stripped = project.split('\\')[-1]
+            if curplatform == 'Linux': project_stripped = project.split('/')[-1] # Linux
+            elif curplatform == 'Windows': project_stripped = project.split('\\')[-1] # Windows
 
             log("---------------------", logfile)
-            log(f"[{project_stripped}]", logfile)
+            log(f"[{target}]\t[{project_stripped}]", logfile)
             os.chdir(project) # Need to us os.chdir to set working directory of subprocesses
 
             for board in boards:
-                log(f"\t[{board}]", logfile)
+                buildlog = f"{target}_{board}_{project_stripped}.log"
                 success = True
 
                 # Test build (make all)
                 build_cmd = f"make all TARGET={target} MAXIM_PATH={MAXIM_PATH} BOARD={board} MAKE=make"
-
-                log(f"{timestamp()}\t\t[BUILD]", logfile) # Log build command
                 res = ps(build_cmd, env=env) # Run build command
 
                 # Error check build command
                 if res.returncode != 0:
                     # Fail
-                    success = False
-                    buildlog = f"{target}{board}_{project_stripped}.log"
-                    log(f"{timestamp()}\t\t[FAILED] Return code {res.returncode}.  See buildlogs/{buildlog}", logfile)            
+                    success = False                    
+                    log(f"{timestamp()}[{board}] --- [BUILD]\t[FAILED] Return code {res.returncode}.  See buildlogs/{buildlog}", logfile)            
                     
                     # Log detailed output to separate output file
                     with open(f"{LOG_DIR}/buildlogs/{buildlog}", 'w') as f:
@@ -184,31 +200,34 @@ def test(targets=None, boards=None, projects=None):
                         for line in str(res.stdout + res.stderr, encoding="ASCII").splitlines():
                             f.write(line + '\n')
 
-                else:
-                    log(f"{timestamp()}\t\t[SUCCESS] {round(duration, 4)}s", logfile)   
+                else: log(f"{timestamp()}[{board}] --- [BUILD]\t[SUCCESS] {round(duration, 4)}s", logfile)                
 
                 # Test clean (make clean)
                 clean_cmd = f"make clean TARGET={target} MAXIM_PATH={MAXIM_PATH} BOARD={board} MAKE=make"
-
-                log(f"{timestamp()}\t\t[CLEAN]", logfile) # Log build command
                 res = ps(clean_cmd, env=env) # Run clean command
 
                 # Error check clean command
                 if res.returncode != 0:
-                    log(f"{timestamp()}\t\t[FAILED] {res.stderr}", logfile)
+                    log(f"{timestamp()}[{board}] --- [CLEAN]\t[SUCCESS] {str(res.stderr, encoding='ASCII')}", logfile)
                     success = False
-                else:
-                    log(f"{timestamp()}\t\t[SUCCESS] {round(duration, 4)}s", logfile)             
+                else: log(f"{timestamp()}[{board}] --- [CLEAN]\t[SUCCESS] {round(duration, 4)}s", logfile)
 
                 # Add any failed projects to running list
-                if not success and project not in failed: failed.append(project)
+                project_info = {
+                    "target":target,
+                    "project":project_stripped,
+                    "board":board,
+                    "path":project,
+                    "logfile":f"buildlogs/{buildlog}"
+                    }
+                if not success and project_info not in failed: failed.append(project_info)
                 count += 1
 
         log("====================", logfile)
 
-    log(f"[SUMMARY] Tested {count} projects.  {count - len(failed)}/{count} succeeded.  Failed projects: ")
-    for p in failed:
-        log(p, logfile)
+    log(f"[SUMMARY] Tested {count} projects.  {count - len(failed)}/{count} succeeded.  Failed projects: ", logfile)
+    for pinfo in failed:
+        log(f"[{pinfo['target']}] {pinfo['project']} for {pinfo['board']}...  see {pinfo['logfile']}", logfile)
 
 if __name__ == "__main__":
     test(targets=["MAX78000"])
