3d3730a26e34d3ff988e6f2558a1178d299c8902
Jake Carter <jhcarter.co@gmail.com>
2022-01-12 16:53:27 -0600

    Use project_name across all tasks

---
+++ MaximSDK/New_Project/build/pb.d
@@ -0,0 +1,24 @@
+c:/Users/Jake.Carter/proj/Active/VSCode-Maxim/MaximSDK/New_Project/build/pb.o: \
+ c:\maximsdk\libraries\boards\max78000\source\pb.c \
+ c:\maximsdk\tools\gnutools\lib\gcc\arm-none-eabi\9.2.1\include\stddef.h \
+ c:/MaximSDK/Libraries/PeriphDrivers/Include/MAX78000/mxc_device.h \
+ c:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX78000/Include/max78000.h \
+ c:\maximsdk\tools\gnutools\lib\gcc\arm-none-eabi\9.2.1\include\stdint.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\stdint.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\machine\_default_types.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\features.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\_newlib_version.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\_intsup.h \
+ c:\maximsdk\tools\gnutools\arm-none-eabi\include\sys\_stdint.h \
+ c:/MaximSDK/Libraries/CMSIS/Include/core_cm4.h \
+ c:/MaximSDK/Libraries/CMSIS/Include/core_cmInstr.h \
+ c:/MaximSDK/Libraries/CMSIS/Include/core_cmFunc.h \
+ c:/MaximSDK/Libraries/CMSIS/Include/core_cm4_simd.h \
+ c:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX78000/Include/system_max78000.h \
+ c:/MaximSDK/Libraries/PeriphDrivers/Include/MAX78000/mxc_errors.h \
+ c:/MaximSDK/Libraries/PeriphDrivers/Include/MAX78000/mxc_pins.h \
+ c:/MaximSDK/Libraries/PeriphDrivers/Include/MAX78000/gpio.h \
+ c:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX78000/Include/gpio_regs.h \
+ c:/MaximSDK/Libraries/PeriphDrivers/Include/MAX78000/mxc_assert.h \
+ c:\maximsdk\libraries\boards\max78000\include\pb.h \
+ c:/MaximSDK/Libraries/PeriphDrivers/Include/MAX78000/gpio.h
---
+++ MaximSDK/New_Project/build/startup_max78000.d
@@ -0,0 +1,2 @@
+c:/Users/Jake.Carter/proj/Active/VSCode-Maxim/MaximSDK/New_Project/build/startup_max78000.o: \
+ c:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX78000/Source/GCC/startup_max78000.S
