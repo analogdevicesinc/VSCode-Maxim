e56e503dc02e97204a760f672e980e5e14f73471
Jake Carter <jake.carter@maximintegrated.com>
2022-02-09 22:05:02 -0600

    Update generator binaries and setup shell script to improve compatabilities with Github repos

--- dist/Ubuntu-20-04LTS/setup.sh
+++ dist/Ubuntu-20-04LTS/setup.sh
@@ -1,14 +1,19 @@
 # VSCode-Maxim setup script.  **Run as user with non-elevated privileges**, ex:  bash setup.sh
 
 SCRIPTDIR=$(cd $(dirname $0) && pwd)
+echo "Running setup.sh from $SCRIPTDIR"
 
-if [ -f /etc/profile.d/maximsdk-env.sh ]; then
-	source /etc/profile.d/maximsdk-env.sh
+ENV_FILE=/etc/profile.d/maximsdk-env.sh
+
+# Check for existence of environment variable shell script on startup so we can refresh if needed
+if [ -f $ENV_FILE ]; then
+	source $ENV_FILE
 else
 	echo "Failed to find /etc/profile.d/maximsdk-env.sh"
 fi
 
-if [ ! -f ./MaintenanceTool.dat ]; then
+VERIFICATION_DIR=Libraries/CMSIS/Device/Maxim
+if [ ! -d ./$VERIFICATION_DIR ]; then
 	# Running script outside of MaximSDK
 	echo "This script is not located inside the root directory of a MaximSDK installation..."
 	
@@ -31,7 +36,7 @@ if [ ! -f ./MaintenanceTool.dat ]; then
 		if [ "$YN" = "y" ] || [ "$YN" = "Y" ]; then
 			echo "Verifying..."
 		elif [ "$YN" = "n" ] || [ "$YN" = "N" ]; then	
-			read -p "Enter the full path of the MaximSDK installation (ex: /home/username/MaximSDK):" MAXIM_PATH
+			read -p "Enter the full path of the latest MaximSDK installation (ex: /home/username/MaximSDK):" MAXIM_PATH
 			echo "Verifying..."
 		else
 			exit -1
@@ -43,26 +48,25 @@ else
 fi
 
 # Final verify
-if [ ! -f $MAXIM_PATH/MaintenanceTool.dat ]; then
+if [ ! -d $MAXIM_PATH/$VERIFICATION_DIR ]; then
 	echo "Verification of MaximSDK installation failed.  Failed to find expected files."
 	exit -2
 else
 	echo "Verified SDK location!"
 fi
 
-# Add MAXIM_PATH to environment variables
-ENV_FILE=/etc/profile.d/maximsdk-env.sh
+# Add MAXIM_PATH to system environment variables
 echo "Adding MAXIM_PATH environment variable..."
 if [ -f $ENV_FILE ]; then
 	# Update existing file
 	echo "Updating $ENV_FILE ..."
 	if [ -z "$(sudo grep "export MAXIM_PATH=" $ENV_FILE)" ]; then
-		# If grep search for MAXIM_PATH in ~/.bashrc returns empty string (-z)
+		# If grep search for MAXIM_PATH returns empty string (-z)
 		echo "Setting MAXIM_PATH=$MAXIM_PATH"
 		sudo sed -i~ "1iexport MAXIM_PATH=${MAXIM_PATH}" $ENV_FILE # Insert line at beginning (1i) and create backup (-i~)
 	else
 		# Update MAXIM_PATH instead of adding new line
-		echo "$ENV_FILE already sets MAXIM_PATH.  Updating to MAXIM_PATH=$MAXIM_PATH"
+		echo "$ENV_FILE already sets MAXIM_PATH.  Updating to use MAXIM_PATH=$MAXIM_PATH"
 		sudo sed -i~ "s:.*export MAXIM_PATH=.*:export MAXIM_PATH=$MAXIM_PATH:" $ENV_FILE # search and replace (s):pattern (.*=all):with this:
 	fi
 
@@ -75,16 +79,21 @@ else
 fi
 
 # Run updates.sh
-echo "Checking for updates..."
-cd $MAXIM_PATH && bash updates.sh
-cd $SCRIPTDIR
+if [ -f $MAXIM_PATH/updates.sh ]; then
+	echo "Checking for updates..."
+	cd $MAXIM_PATH && bash updates.sh
+	cd $SCRIPTDIR
+fi
+
 
 # Get newer GCC
 GCC_VERSION="10.3.1"
+ARCH=$(arch)
+PACKAGE_STRING="10.3-2021.10"
 GCC_PATH="$MAXIM_PATH/Tools/GNUTools/gcc-arm-none-eabi-$GCC_VERSION"
-ARM_LINK="https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2"
-PACKAGE_NAME="gcc-arm-none-eabi-10.3-2021.10"
-echo "Getting Arm embedded toolchain v$GCC_VERSION..."
+ARM_LINK="https://developer.arm.com/-/media/Files/downloads/gnu-rm/$PACKAGE_STRING/gcc-arm-none-eabi-$PACKAGE_STRING-$ARCH-linux.tar.bz2"
+
+echo "Getting Arm embedded toolchain v$GCC_VERSION from $ARM_LINK..."
 if [ ! -d $GCC_PATH ]; then
 	wget -O gcc-arm.tar.bz2 $ARM_LINK
 	mkdir -p $GCC_PATH
