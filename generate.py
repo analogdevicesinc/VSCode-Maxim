129461e31779e00b531f9275dcb8e292a39d861d
Jake Carter <jake.carter@maximintegrated.com>
2022-02-15 17:10:15 -0600

    Use utils.py

--- generate.py
+++ generate.py
@@ -3,21 +3,7 @@ import shutil
 import argparse
 import platform
 from subprocess import run
-import json
-
-def parse_json(filename):
-    """
-    Parse values from a json file into a template-friendly (all-caps keys) dictionary
-    """
-    f = open(filename, "r")
-    defaults = json.load(f)
-
-    # Convert key values to uppercase for easier template parsing
-    keys = list(defaults.keys()) # Keys are changing on the fly, so can't use a view object
-    for k in keys:
-        defaults[k.upper()] = defaults.pop(k)
-    
-    return defaults
+from utils import parse_json
 
 # Load default values for template from master "inject" folder so that we don't have to maintain multiple copies of the settings
 defaults = parse_json("MaximSDK/Inject/.vscode/settings.json")
